d4e754ca990a6940f012ba1bfe7440c934d6f95c
Utilizador <Utilizador@WIN-MP3JADQD58A.local>
2020-10-16 12:18:51 +0100

    Update ficha6.php

---
+++ ficha6.php
@@ -0,0 +1,32 @@
+<?php
+$a=23;
+$b=45;
+$soma=$a+$b;
+echo $a."+" . $b ."=".$soma;
+
+
+$a= 10;
+$b= 12;
+$c=14;
+echo"<br>";
+$media=($a+$b+$c)/3;
+echo "10+12+14/3=".$media;
+
+
+$x=30;
+$y=20;
+$quociente = $x/$y;
+$resto = ($x % $y);
+echo "<br><br>20/30=".$quociente;
+echo "<br>resto=".$resto;
+
+echo "<br>O João foi à oficina e a
+reparação do carro é de 78,00 € sem IVA.";
+$preço=78;
+$iva=0.23;
+$soma=$preço*$iva;
+$quociente=$preço+$iva+$soma;
+echo "<br> O João pagará ".$quociente;
+
+?>
+
